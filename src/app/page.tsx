249e7fc05dc26fae99a56b29c8292773265128d4
Cesar Garcia <cesargarcia@Cesars-MBP-2.hsd1.or.comcast.net>
2025-07-28 21:00:09 -0700

    added body

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,14 @@ export default function Home() {
         Cesar Garcia Portfolio
       </h1>
      </header>
+     <div>
+      <p>
+        I had watched so many guys in the chairs be the coolest characters in the shows or movies. I worked 
+        to become that guy in the chair with learning python and React. I became a React developer intern for a 
+        few months and which led to my software engineer experienc. That position made me feel like the guy in the chair 
+        because I was constantly learning about new technologies and maintaining my current knowledge.  
+      </p>
+     </div>
     </div>
   );
 }
